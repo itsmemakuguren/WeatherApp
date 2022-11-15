174ea083393d47facbedad9071dc9da54a62cd53
iMAC 5 MAC Lab <>
2022-11-15 14:44:30 +0800

    Finished WeatherApp

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from "expo-location";
+
+import DateTime from "./components/DateTime";
+import WeatherScroll from "./components/WeatherScroll";
+
+const API_KEY = "49cc8c821cd2aff9af04c9f98c36eb74";
+
+const imgBG = require("./assets/images.jpg");
 
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        //console.log("13.4074", "123.3731");
+        fetchDataFromApi("13.4074", "123.3731");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      //console.log(location.coords.latitude, location.coords.longitude);
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, []);
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if (latitude && longitude) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+      )
+
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setData(data);
+      });
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source = {imgBG} style = {styles.image}>
+
+        <DateTime
+          current = {data.current}
+          timezone = {data.timezone}
+          lat = {data.lat}
+          lon = {data.lon}
+        />
+        <WeatherScroll weatherData = {data.daily} />
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +62,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    //alignItems: "center",
+    //justifyContent: "center",
   },
 });
